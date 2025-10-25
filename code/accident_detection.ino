51fb8472d084d44e89d779f44529e19f95b71e94
Satish Babu <satishchowdary5089@gmail.com>
2025-10-25 20:45:13 +0530

    accident_detection.ino

--- code/accident_detection.ino
+++ code/accident_detection.ino
@@ -1,43 +1,57 @@
 #include <SoftwareSerial.h>
-SoftwareSerial gsm(9,10);
-SoftwareSerial gps(4,5);
+#include <TinyGPS++.h>
 
-int vibrationPin = 7;
-bool accidentDetected = false;
+SoftwareSerial gpsSerial(4, 3);   // RX, TX for GPS
+SoftwareSerial gsmSerial(7, 8);   // RX, TX for GSM
+TinyGPSPlus gps;
+
+String phoneNumber = "+919876543210";
 
 void setup() {
-  pinMode(vibrationPin, INPUT);
   Serial.begin(9600);
-  gsm.begin(9600);
-  gps.begin(9600);
-  Serial.println("System ready...");
+  gpsSerial.begin(9600);
+  gsmSerial.begin(9600);
+  delay(2000);
+
+  gsmSerial.println("AT");
+  delay(1000);
+  gsmSerial.println("AT+CMGF=1");  // SMS text mode
+  delay(1000);
+
+  Serial.println("System Ready. Waiting for GPS fix...");
 }
 
 void loop() {
-  int vibration = digitalRead(vibrationPin);
-  if(vibration == HIGH && accidentDetected == false) {
-    accidentDetected = true;
-    Serial.println("Accident detected!");
-    
-    String location = getGPSLocation();
-    sendAlert(location);
+  while (gpsSerial.available() > 0) {
+    gps.encode(gpsSerial.read());
+  }
+
+  if (gps.location.isUpdated()) {
+    double latitude = gps.location.lat();
+    double longitude = gps.location.lng();
+
+    String googleMapLink = "https://www.google.com/maps/search/?api=1&query=";
+    googleMapLink += String(latitude, 6);
+    googleMapLink += ",";
+    googleMapLink += String(longitude, 6);
+
+    sendSMS(phoneNumber, googleMapLink);
     delay(10000);
   }
 }
 
-String getGPSLocation() {
-  String lat = "17.3850"; // sample data
-  String lon = "78.4867"; // sample data
-  return "Lat:" + lat + ", Lon:" + lon;
-}
+void sendSMS(String number, String message) {
+  gsmSerial.println("AT+CMGF=1");
+  delay(500);
+  gsmSerial.print("AT+CMGS=\"");
+  gsmSerial.print(number);
+  gsmSerial.println("\"");
+  delay(500);
+  gsmSerial.print("Accident Alert! Vehicle Location:\n");
+  gsmSerial.print(message);
+  delay(500);
+  gsmSerial.write(26);  // Ctrl+Z to send
+  delay(2000);
 
-void sendAlert(String location) {
-  gsm.println("AT+CMGF=1");
-  delay(1000);
-  gsm.println("AT+CMGS="+911234567890"");
-  delay(1000);
-  gsm.print("Accident detected! Vehicle location: ");
-  gsm.print(location);
-  gsm.write(26);
-  Serial.println("Alert sent successfully!");
+  Serial.println("SMS Sent: " + message);
 }
